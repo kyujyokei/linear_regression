11859e7027047369c39f65bd29dc64c706036ac0
kyujyokei <kyujyokei@gmail.com>
2018-10-10 15:11:32 -0700

    FIxed the inital settings

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 features = ['date','bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront','view','condition','grade','sqft_above','sqft_basement','yr_built','yr_renovated','zipcode','lat','long','sqft_living15','sqft_lot15']
 
-w_dict = {'date':[],'bedrooms':[],'bathrooms':[],'sqft_living':[],'sqft_lot':[],'floors':[],'waterfront':[],'view':[],'condition':[],'grade':[],'sqft_above':[],'sqft_basement':[],'yr_built':[],'yr_renovated':[],'zipcode':[],'lat':[],'long':[],'sqft_living15':[],'sqft_lot15':[]}
+w_dict = {'date':0,'bedrooms':0,'bathrooms':0,'sqft_living':0,'sqft_lot':0,'floors':0,'waterfront':0,'view':0,'condition':0,'grade':0,'sqft_above':0,'sqft_basement':0,'yr_built':0,'yr_renovated':0,'zipcode':0,'lat':0,'long':0,'sqft_living15':0,'sqft_lot15':0}
 
 D = pd.read_csv("PA1_train.csv") # /// if index_column set true, will exclude the index column and not put into consideration
 # print(D)
@@ -25,21 +25,24 @@ y_data = D[colnames[-1:]] # the prices only
 # for i in features:
     # print(w_dict[i])
 
+# stores initial w and b
 b = -120 #initial b
-w = [ -4 for _ in range(0,len(x_data))] # an array of all the Ws for each feature
+for i in features: w_dict[i] = -4
 
 lr = 0.0001 #learning rate
 iteration = 100000
 
-b_history = [b]
-w_history = [w]
+# b_history = [b]
+# w_history = [w_dict]
 
-for i in range (iteration):
-    b_grad = 0.0
-    w_grad = [0.0 for _ in range(0,len(x_data))]
-    for j in range(len(x_data)):
-        b_grad = b_grad - 2.0 * (y_data[j] - b - w * x_data[j]) * 1.0
-        for n in features:
-            w_grad = w_grad - 2.0 * (y_data[j] - b - w * x_data[j]) * x_data[j]
-            w = w - lr * w_grad
-        b = b - lr * b_grad
+# print(w_dict)
+
+# for i in range (iteration):
+#     b_grad = 0.0
+#     w_grad = [0.0 for _ in range(0,len(x_data))]
+#     for j in range(len(x_data)):
+#         b_grad = b_grad - 2.0 * (y_data[j] - b - w * x_data[j]) * 1.0
+#         for n in features:
+#             w_grad = w_grad - 2.0 * (y_data[j] - b - w * x_data[j]) * x_data[j]
+#             w = w - lr * w_grad
+#         b = b - lr * b_grad

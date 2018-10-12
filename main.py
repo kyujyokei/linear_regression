33adef706abc5c3623735000182ac769e9ba0406
kyujyokei <kyujyokei@gmail.com>
2018-10-12 16:23:03 -0700

    A working model but seems a bit off.

--- main.py
+++ main.py
@@ -1,52 +1,125 @@
-import pandas as pd
 import numpy as np
-import csv
 
 
-features = ['day','month','year','bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront','view','condition','grade','sqft_above','sqft_basement','yr_built','yr_renovated','zipcode','lat','long','sqft_living15','sqft_lot15']
 
-w_dict = {'day':0,'month':0,'year':0,'bedrooms':0,'bathrooms':0,'sqft_living':0,'sqft_lot':0,'floors':0,'waterfront':0,'view':0,'condition':0,'grade':0,'sqft_above':0,'sqft_basement':0,'yr_built':0,'yr_renovated':0,'zipcode':0,'lat':0,'long':0,'sqft_living15':0,'sqft_lot15':0}
+data = np.genfromtxt("PA1_train.csv", dtype=np.str, delimiter=",")
 
-dateparse = lambda x: pd.datetime.strptime(x, '%m/%d/%Y')
-D = pd.read_csv("PA1_train.csv",parse_dates=['date'], date_parser=dateparse) # /// if index_column set true, will exclude the index column and not put into consideration
+data = np.delete(data, 0, axis=0)
+data = np.delete(data, 1, axis=1)
 
-def splitDate(df):
-    # df = df.copy()
-    df['year'] = pd.DatetimeIndex(df['date']).year
-    df['month'] = pd.DatetimeIndex(df['date']).month
-    df['day'] = pd.DatetimeIndex(df['date']).day
-    return df
+data = np.insert(data, 2, values='year', axis=1)
+data = np.insert(data, 2, values='day', axis=1)
+data = np.insert(data, 2, values='month', axis=1)
 
-colnames = D.columns.values.tolist() # colnames is an array
-x_data = D[colnames[2:-1]] # from date to the end, excluding id
-y_data = D[colnames[-1:]] # the prices only
+# read data
 
-splitDate(x_data)
+for i in range(len(data)):
+    # print(data[i])
+    date = data[i][1].split('/')
+    data[i][2], data[i][3], data[i][4] = date[0], date[1], date[2]
 
+    if data[i][17] == '0':
+        data[i][17] = data[i][16]
 
-# stores initial w and b
-b = -120 #initial b
-for i in features: w_dict[i] = -4.0
+    if data[i][18][:3] == '981':
+        data[i][18] = 1
+    else:
+        data[i][18] = 0
 
-lr = 0.0001 #learning rate
-iteration = 1
-
-for i in range (iteration):
-    b_grad = 0.0
-    w_grad = [0.0 for _ in range(0, len(x_data))]
-
-    for k, n in enumerate(features):
-
-        for j in range(len(x_data)):
-
-            b_grad =  (  b + w_dict[n] * x_data[n][j]) - y_data['price'][j] * 1.0
-            w_grad[k] = w_grad[k] - (2.0 * (y_data['price'][j] - b - w_dict[n] * x_data[n][j])) * x_data[n][j]
-
-        w_dict[n] = w_dict[n] - lr * w_grad[k]
+data = np.delete(data, 1, axis=1)
+data = data.astype(float)
+label = data[:, -1:]
+data = data[:, :-1]
 
 
-    b = b - lr * b_grad
 
 
-print("b:", b)
-print("w:", w_dict)
+def calc_percentage(data):
+    dic = {}
+    for i in data:
+        dic[i] = dic[i] + 1 if i in dic else 1
+    for  j in dic:
+        dic[j] = format((j == data).sum() / len(data), '.3f')
+    return dic
+
+
+# print("Mean: ", np.mean(data, axis=0))
+# print("Standard Deviation: ", np.std(data, axis=0))
+# print("Range: ", np.ptp(data, axis=0))
+# print(np.min(data, axis=0))
+# print(np.max(data, axis=0))
+# print("Waterfront = 1 : ", (0 < data[:, 9]).sum() / len(data[:, 9]))
+# print("", (0 == data[:, 9]).sum() / len(data[:, 9]))
+
+
+# print("", np.percentile(data, 1))
+# print(data[0])
+
+print("Waterfront:  ", calc_percentage(data[:, 9]))
+print("Condition:   ", calc_percentage(data[:, 11]))
+print("Grade:       ", calc_percentage(data[:, 12]))
+print("ZIP Code:    ", calc_percentage(data[:, 17]))
+
+
+
+""" normalization"""
+
+# for i in range(len(data[0])-1):
+#     data[:, i] = data[:, i] / np.linalg.norm(data[:, i])
+# # print(data[0])
+
+def normalize(v):
+    """
+    normalize a vector
+    :param v:
+    :return:
+    """
+    norm = np.linalg.norm(v)
+    if norm == 0:
+       return v
+    return v / norm
+
+
+def normalize_matrix(data):
+    """
+    normalize a matrix according to each column
+    :param data: matrix
+    :return:
+    """
+    if data is None or len(data) == 0:
+        return data
+    for i in range(len(data[0])):
+        data[:, i] = normalize(data[:, i])
+    return data
+
+
+data = normalize_matrix(data)
+
+data_t = np.transpose(data)
+# print(data_t[1])
+
+
+lr = 0.001
+lam = 0.1
+
+w = np.zeros((len(data[0]), 1))
+
+for iter in range(10000):
+
+    predict_y = np.matmul(data, w)
+
+    grad = np.matmul(data_t, label - predict_y) + lam * w
+
+    w += lr * grad
+
+predict = np.matmul(data, w)
+
+print("W: ", w)
+
+sse = (((label - predict).sum())**2)*0.5
+
+
+print("Predict: ", predict, " Truth: ", label[:10])
+print(sse)
+
+
